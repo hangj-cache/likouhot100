fc5ce86470e38587de2893eee92f973fc4bc57d9
Hang Jiang <hang_jiang1835@163.com>
2025-06-27 00:59:03 +0800

    626

--- src/main/java/com/Hang/MaxArea.java
+++ src/main/java/com/Hang/MaxArea.java
@@ -1,14 +1,36 @@
 package com.Hang;
 
+import java.util.Scanner;
+
 public class MaxArea {
 
     public static int maxArea(int[] height){
         int n = height.length;
         int l = 0, r = n - 1;
-
-
+        int maxA = 0, curA = 0;
         while(l < r){
-
+            curA = (r - l) * Math.min(height[l], height[r]);
+            maxA = Math.max(maxA,curA);
+            if(height[l] < height[r]){
+                l++;
+            }else{
+                r--;
+            }
         }
+
+        return maxA;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] strs = str.substring(1,str.length()-1).split(",\\s*");
+        int[] nums = new int[strs.length];
+        for(int i = 0; i < strs.length; i++){
+            nums[i] = Integer.parseInt(strs[i]);
+        }
+
+        int ans = maxArea(nums);
+        System.out.println(ans);
     }
 }
---
+++ src/main/java/com/Hang/ThreeSum.java
@@ -0,0 +1,64 @@
+package com.Hang;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ThreeSum {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int num = nums[i];
+
+            int l = i + 1, r = n - 1;
+            while(l < r) {
+                if(num+ nums[l] + nums[r] == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(num);
+                    list.add(nums[l]);
+                    list.add(nums[r]);
+                    res.add(list);
+                    l++;
+                    r--;
+                    while(l < r && nums[l] == nums[l-1]){
+                        l++;
+                    }
+
+                    while(l < r && nums[r] == nums[r+1]){
+                        r--;
+                    }
+
+
+                }else if(num+ nums[l] + nums[r] < 0) {
+                    l++;
+                }else{
+                    r--;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] strs = str.substring(1, str.length() - 1).split(",\\s*");
+
+        int[] nums = new int[strs.length];
+        for(int i = 0; i < strs.length; i++) {
+            nums[i] = Integer.parseInt(strs[i]);
+        }
+
+        List<List<Integer>> ans = threeSum(nums);
+
+        System.out.println(ans);
+    }
+}
