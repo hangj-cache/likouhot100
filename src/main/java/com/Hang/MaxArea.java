74e5455571248d08c485355e340482c2ca13b0ea
zhengfei <zhengfei@email>
2025-06-26 19:09:32 +0800

    626

---
+++ src/main/java/com/Hang/MaxArea.java
@@ -0,0 +1,14 @@
+package com.Hang;
+
+public class MaxArea {
+
+    public static int maxArea(int[] height){
+        int n = height.length;
+        int l = 0, r = n - 1;
+
+
+        while(l < r){
+
+        }
+    }
+}
