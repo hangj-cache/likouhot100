22a4b92e8b84d362990c1c42c63fe0f53c858737
zhengfei <zhengfei@email>
2025-07-01 17:49:50 +0800

    71

--- src/main/java/com/Hang/hard/SearchMatrix.java
+++ src/main/java/com/Hang/hard/SearchMatrix.java
@@ -1,11 +1,34 @@
-package com.Hang;
+package com.Hang.hard;
 
 import java.util.Scanner;
 
+/**
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+ *
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ *
+ * 肯定也是要两层循环的，只是可以跳过一部分元素的检索
+ */
 public class SearchMatrix {
 
     public static boolean searchMatrix(int[][] matrix, int target) {
 
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = 0; j < n; j++){
+                if(matrix[j][i] == target){
+                    return true;
+                }else if(matrix[j][i] > target){
+                    break;
+                }else{
+                    continue;
+                }
+            }
+        }
+        return false;
 
 
     }
